dc2ff2dcd5981899f876184e7940660b21713105
Dipak_Aiwale <84844273+DipakAiwale007@users.noreply.github.com>
2024-12-30 15:49:51 +0000

    dataTypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 80551122
+let accountEmail = "dipak@gmail.com"
+var accountPassword ="12345678"
+accountCity = "colbiya"
+let accountStates;
+
+//accountEmail = 2
+
+accountEmail ="Chaku@gs.com"
+accountPassword = "12121212"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+bacause of issue in block scope and functional scope
+*/
+console.table([accountId , accountEmail , accountPassword , accountCity , accountStates])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // trat all JS codr as newer version
+
+// alert(3+3) //  we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Dipak")
+
+
+
+let name = "Dipak"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//dtring => ""
+//boolean =>true /false
+// null => standalone values
+// undefined =>
+// symbol => uniqe
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
