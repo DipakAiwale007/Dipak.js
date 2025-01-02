a0ae810a805ecbc204b495abc62a23291741e869
Dipak_Aiwale <84844273+DipakAiwale007@users.noreply.github.com>
2025-01-02 05:18:18 +0000

    string

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Dipak"
+const repoCount = 50
+
+console.log (`Hello my name is ${name} and my repo Count is ${repoCount}`);
+const gameName = new String ('Dipak')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
